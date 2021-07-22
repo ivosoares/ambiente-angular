e94b7881f570d9c2de763e8f5eea90d238b2165a
Ivonaldo Soares <vonaldojrsoares@gmail.com>
2021-07-21 21:53:11 -0300

    Adiciona exemplo de diretivas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ambiente-angular';
+export class AppComponent implements OnInit {
+  count = 0;
+  nome = 'Ivonaldo Junior Escobar Soares';
+  text = '';
+
+  pessoas = [
+    {
+      nome: "Ivonaldo",
+      sobrenome: "Soares"
+    },
+    {
+      nome: "Maria",
+      sobrenome: "Silva"
+    },
+    {
+      nome: "Joao",
+      sobrenome: "Pereira"
+    },
+    {
+      nome: "Marcio",
+      sobrenome: "Santos"
+    }
+  ];
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    console.log(this.pessoas);
+    let interval = setInterval(() => {
+      this.count++;
+      if(this.count === 10) {
+        clearInterval(interval);
+      }
+    }, 1000)
+  }
+
+  clicou(nome: string): void {
+    console.log('Clicou em min', nome)
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
